f1126d603bf6e880220c72875537e888c2e578d6
Khai <khairulzamidi2000@gmail.com>
2023-12-17 00:05:44 +0800

    Initial commit

--- src/components/home/HomeSection.js
+++ src/components/home/HomeSection.js
@@ -12,16 +12,16 @@ const HomeSection = () => {
           <Row id="home-inner">
             <Col lg={8} className="d-none d-lg-block">
               <h1 className="display-4">
-                Build <strong>social profiles</strong> and gain revenue{" "}
-                <strong>profit</strong>
+                Framing <strong>Life's Beauty</strong> Through The Lens{" "}
               </h1>
               <div className="d-flex">
                 <div className="p-4 align-self-start">
                   <FontAwesomeIcon icon={["fas", "check"]} size="lg" />
                 </div>
                 <div className="p-4 align-self end">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Minus, labore veniam consequuntur corporis repellat mollitia?
+                Step into a world where the beauty of everyday life is captured 
+                through a collection of photographs, each one telling a story, 
+                freezing a fleeting moment, and transforming it into a timeless memory.
                 </div>
               </div>
               <div className="d-flex">
